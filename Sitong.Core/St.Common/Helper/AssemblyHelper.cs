22119f445e93f7fa48380b68e2018fa7bc4bb317
SiTong <15273362112@163.com>
2020-09-17 00:20:19 +0800

    1

---
+++ Sitong.Core/St.Common/Helper/AssemblyHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace St.Common.Helper
+{
+    /// <summary>
+    /// 程序集封装
+    /// </summary>
+    public class AssemblyHelper
+    {
+        /// <summary>
+        /// 获取项目所有的程序集
+        /// </summary>
+        /// <returns></returns>
+        public static List<Assembly> GetAssemblys()
+        {
+            string[] filters =
+           {
+                "mscorlib",
+                "netstandard",
+                "dotnet",
+                "api-ms-win-core",
+                "runtime.",
+                "System",
+                "Microsoft",
+                "Window",
+            };
+            List<Assembly> list = new List<Assembly>();
+            var deps = DependencyContext.Default;
+            //排除所有的系统程序集、Nuget下载包
+            var libs = deps.CompileLibraries.Where(op => !op.Serviceable && op.Type != "package" && !filters.Any(op.Name.StartsWith));
+            foreach (var lib in libs)
+            {
+                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(lib.Name));
+                list.Add(assembly);
+            }
+            return list;
+        }
+    }
+}
--- Sitong.Core/St.Host.API/Program.cs
+++ Sitong.Core/St.Host.API/Program.cs
@@ -1,7 +1,15 @@
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Hosting;
+using St.Application.Infrastruct.Identity;
+using St.Common.Helper;
+using St.Extensions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace St.Host.API
 {
@@ -17,6 +25,20 @@ namespace St.Host.API
                 "哈姆达姆 阿卡棒 咿呀哟\r\n" +
                 "玛卡巴卡 阿卡哇卡\r\n" +
                 "米卡马卡 姆\r\n");
+
+            var result = AssemblyHelper.GetAssemblys().SelectMany(op => op.GetTypes())
+                .ToArray()
+                .Distinct();
+            var abstracts = result.Where(op => !op.IsClass && op.IsAbstract && op.IsInterface).ToArray();
+            // TODO: 实现生命周期管理，默认全体Scoped。可根据特性自定义注入服务
+            foreach (var item in abstracts)
+            {
+                var singleResult = result.Where(op => /*op.IsClass && !op.IsAbstract && !op.IsInterface &&*/ item.IsAssignableFrom(op)).ToArray();// 注意工厂注入
+                if (singleResult.Length > 0 && singleResult.Length < 2)
+                {
+                    Console.WriteLine($"Interface => { item.Name } \r\n realize => { singleResult[0].Name } \r\n");
+                }
+            }
 
             Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())//注入AutoFac模块
