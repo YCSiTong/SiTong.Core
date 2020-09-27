40a70223908831503b54937dae130dd984cd7c1c
SiTong <15273362112@163.com>
2020-09-28 00:53:22 +0800

    1

---
+++ Sitong.Core/St.AOP/UnitOrWork/UnitOfWorkSave.cs
@@ -0,0 +1,21 @@
+﻿using AspectCore.DynamicProxy;
+using St.DoMain.UnitOfWork;
+using System.Threading.Tasks;
+
+namespace St.AOP.UnitOrWork
+{
+    /// <summary>
+    /// 全面事务启用
+    /// </summary>
+    public class UnitOfWorkSave : AbstractInterceptorAttribute
+    {
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var unitOfWork = (IUnitOfWork)context.ServiceProvider.GetService(typeof(IUnitOfWork));
+            await unitOfWork.UseTransactionAsync(async () =>
+            {
+                await next(context);
+            });
+        }
+    }
+}
